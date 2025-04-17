3bfa9ba80481b858a168e6f1bcf1874ddad8378d
Hugo Arsénio <hugooaarsenio@gmail.com>
2025-04-17 14:32:16 +0200

    Delete prosthetic_control directory

--- prosthetic_control/actuator_interface.py
+++
@@ -1,9 +0,0 @@
-import serial
-
-arduino = serial.Serial('/dev/ttyACM0', 9600)
-
-def send_command(command):
-    if command == "GRIP":
-        arduino.write(b'G')
-    elif command == "RELEASE":
-        arduino.write(b'R')
